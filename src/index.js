79b2f04f4974ad64426a10102fb376b280f7167d
Akis <b_kalogeropoulos@yahoo.gr>
2021-10-23 16:49:49 +0300

    User authentication

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+require('./models/User');
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(authRoutes);
-
 
 const mongoUri = 'mongodb+srv://admin:passwordpassword@cluster0.nsd0e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 mongoose.connect(mongoUri);
